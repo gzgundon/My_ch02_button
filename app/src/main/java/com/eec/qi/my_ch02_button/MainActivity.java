56bdf31ba64739848bd09309cfac88f949687e98
gzgundon <gz837900w@163.com>
2016-08-08 17:04:13 +0800

    0808

--- app/src/main/java/com/eec/qi/my_ch02_button/MainActivity.java
+++ app/src/main/java/com/eec/qi/my_ch02_button/MainActivity.java
@@ -3,6 +3,7 @@ package com.eec.qi.my_ch02_button;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,16 +18,26 @@ public class MainActivity extends AppCompatActivity {
     int flag=0;
     public void bigger(View v) {
         TextView txv;
+        Button  anniu;
+
         txv= (TextView) findViewById(R.id.txv);
+        anniu= (Button) findViewById(R.id.button);
         if(flag==0) {
             txv.setTextSize(++size);
-            if(size==40)
+
+            if (size == 40) {
                 flag=1;
+                anniu.setText(R.string.btn_suoxiao);
+
+            }
+
         }
         if (flag == 1) {
             txv.setTextSize(--size);
-            if(size==30)
-                flag=0;
+            if(size==30) {
+                flag = 0;
+                anniu.setText(R.string.button_name_fangda);
+            }
         }
 
 
